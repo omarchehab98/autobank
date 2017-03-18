2b848de402df13a4225776ed995b49b2c044d7c8
Omar Chehab <omarchehab98@gmail.com>
2017-03-17 22:26:01 -0400

    storage

--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
 const Mailbox = require('eun-mailbox');
+const Expenses = require('eun-expenses');
 const Filter = require('eun-filter');
 const filterFieldset = require('eun-filter-fieldset');
 const filterRBC = require('eun-filter-rbc');
+
 const isString = require('lodash/isString');
+
 const credentials = require('../credentials.json');
 
-//
-const logIt = result => console.log(result);
-//
+const expenses = new Expenses(credentials.expenses);
 
-const options = {lazy: true};
-const filter = new Filter(options, [
-    [isString, filterFieldset, filterRBC.creditWithdrawal, logIt],
-    [isString, filterFieldset, filterRBC.creditDeposit, logIt],
-    [isString, filterFieldset, filterRBC.chequingWithdrawal, logIt],
-    [isString, filterFieldset, filterRBC.chequingDeposit, logIt]
+const filter = new Filter({}, [
+	[isString, filterFieldset, filterRBC.creditWithdrawal, logIt, expenses.putExpense],
+	[isString, filterFieldset, filterRBC.creditDeposit, logIt, expenses.putIncome],
+	[isString, filterFieldset, filterRBC.chequingWithdrawal, logIt, expenses.putExpense],
+	[isString, filterFieldset, filterRBC.chequingDeposit, logIt, expenses.putIncome]
 ]);
 
-const mailbox = new Mailbox(credentials.mailbox, {markSeen: false});
+const mailbox = new Mailbox(credentials.mailbox);
 mailbox.on('mail', mail => filter(mail.body.text));
+
+function logIt(transaction) {
+	const date = prettyDate(transaction.timestamp);
+	const amount = transaction.amount;
+	console.log(date, amount);
+	return true;
+}
+
+function prettyDate(timestamp) {
+	const validTimestamp = Number(timestamp) * 1000;
+	return new Date(validTimestamp)
+		.toISOString()
+		.substring(0, 10);
+}
