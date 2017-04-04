296cab9f91daff603c249bad88ca799ed6f7e179
Omar Chehab <omarchehab98@gmail.com>
2017-04-03 21:22:32 -0400

    gitignore credentials, supress lint warning

--- src/main.js
+++ src/main.js
@@ -4,23 +4,46 @@ const Filter = require('eun-filter');
 const filterFieldset = require('eun-filter-fieldset');
 const filterRBC = require('eun-filter-rbc');
 
+// Assertions
 const isString = require('lodash/isString');
 
-const credentials = require('../credentials.json');
+// eslint-disable-next-line import/no-unresolved
+const credentials = require('./credentials.json');
 
 const expenses = new Expenses(credentials.expenses);
 
 const filter = new Filter({}, [
-	[isString, filterFieldset, filterRBC.creditWithdrawal, logIt, expenses.putExpense],
-	[isString, filterFieldset, filterRBC.creditDeposit, logIt, expenses.putIncome],
-	[isString, filterFieldset, filterRBC.chequingWithdrawal, logIt, expenses.putExpense],
-	[isString, filterFieldset, filterRBC.chequingDeposit, logIt, expenses.putIncome]
+	[
+		isString,
+		filterFieldset,
+		filterRBC.creditWithdrawal,
+		logTranscation,
+		expenses.putExpense
+	], [
+		isString,
+		filterFieldset,
+		filterRBC.creditDeposit,
+		logTranscation,
+		expenses.putIncome
+	], [
+		isString,
+		filterFieldset,
+		filterRBC.chequingWithdrawal,
+		logTranscation,
+		expenses.putExpense
+	], [
+		isString,
+		filterFieldset,
+		filterRBC.chequingDeposit,
+		logTranscation,
+		expenses.putIncome
+	]
 ]);
 
 const mailbox = new Mailbox(credentials.mailbox);
 mailbox.on('mail', mail => filter(mail.body.text));
 
-function logIt(transaction) {
+function logTranscation(transaction) {
 	const date = prettyDate(transaction.timestamp);
 	const amount = transaction.amount;
 	console.log(date, amount);
